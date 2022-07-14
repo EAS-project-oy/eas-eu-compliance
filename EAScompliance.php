ded3728dba9c5fefadbdd2830836ab26e44b296b
Roman Denisov <roman@bisoft.fi>
2022-07-14 20:03:20 +0300

    WP-91 fix billing_phone field may not present at checkout

--- EAScompliance.php
+++ EAScompliance.php
@@ -983,12 +983,12 @@ function eascompliance_make_eas_api_request_json($currency_conversion = true) {
 	$calc_jreq['recipient_company_vat']   = '';
 	$calc_jreq['delivery_address_line_1'] = $checkout['shipping_address_1'];
 	$calc_jreq['delivery_address_line_2'] = eascompliance_array_get($checkout, 'billing_address_2', '' );//$checkout['shipping_address_2'];
-	$calc_jreq['delivery_city']           = $checkout['shipping_company']    = eascompliance_array_get($checkout,'shipping_city', '');
+	$calc_jreq['delivery_city']           = eascompliance_array_get($checkout,'shipping_city', '');
 	$calc_jreq['delivery_state_province'] = '' === $delivery_state_province ? '' : $delivery_state_province;
 	$calc_jreq['delivery_postal_code']    = $checkout['shipping_postcode'];
 	$calc_jreq['delivery_country']        = $checkout['shipping_country'];
-	$calc_jreq['delivery_phone']          = $checkout['billing_phone'];
-	$calc_jreq['delivery_email']          = $checkout['billing_email'];
+	$calc_jreq['delivery_phone']          = eascompliance_array_get($checkout, 'billing_phone', '');
+	$calc_jreq['delivery_email']          = eascompliance_array_get($checkout, 'billing_email', '');
 
 	$jdebug['step'] = 'fill json request with cart data';
 	$countries = array_flip(WORLD_COUNTRIES);
