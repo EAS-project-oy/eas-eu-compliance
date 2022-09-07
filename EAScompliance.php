139786c68894bbf1f5903bc73a853ef9c79b5baf
MIkhail Muromskii <73703648+easproject@users.noreply.github.com>
2022-09-07 16:40:13 +0300

    Once again message updated

--- EAScompliance.php
+++ EAScompliance.php
@@ -4789,7 +4789,7 @@ function eascompliance_woocommerce_settings_start()
         }
 
         if ('hidden' === get_option('woocommerce_checkout_phone_field')) {
-            WC_Admin_Settings::add_error(EAS_TR('We can’t proceed landed cost calculation because required delivery Phone field is disabled. Please visit Appearance → Customize page. Select "WooCommerce" then "Checkout" and change settings for the Phone field settings to Optional or Required.'));
+            WC_Admin_Settings::add_error(EAS_TR('Landed cost calculation will be prohibbited for EU on checkout because required delivery Phone field is disabled. Please visit Appearance → Customize page. Select "WooCommerce" then "Checkout" and change settings for the Phone field settings to Optional or Required.'));
         }
 
     } catch (Exception $ex) {
