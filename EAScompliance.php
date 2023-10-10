876256a5bf838978c78939ee18e8e59526168773
MIkhail Muromskii <73703648+easproject@users.noreply.github.com>
2023-10-10 16:22:36 +0300

    supported version updated

--- EAScompliance.php
+++ EAScompliance.php
@@ -7,10 +7,10 @@
  * Text Domain: eas-eu-compliance
  * Domain Path: /languages
  * Version: 1.4.86
- * Tested up to 6.2
+ * Tested up to 6.3
  * WC requires at least: 4.8.0
  * Requires at least: 4.8.0
- * WC tested up to: 7.8.1
+ * WC tested up to: 8.1.1
  * Requires PHP: 5.6
  * License: GPL2
  *
