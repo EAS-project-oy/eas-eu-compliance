533707b50e74ceb9aa14fa6dc8f6610825662acd
Roman Denisov <roman@bisoft.fi>
2023-10-12 14:25:39 +0300

    Version: 1.4.87

--- EAScompliance.php
+++ EAScompliance.php
@@ -6,7 +6,7 @@
  * Author URI: https://easproject.com/about-us/
  * Text Domain: eas-eu-compliance
  * Domain Path: /languages
- * Version: 1.4.86
+ * Version: 1.4.87
  * Tested up to 6.3
  * WC requires at least: 4.8.0
  * Requires at least: 4.8.0
